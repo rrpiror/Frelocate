0828ff24ebbec51e442a97b2289c04df6f41a600
Rob Prior <rrprior@gmail.com>
2017-12-14 22:20:19 +0000

    changing ui for savings calculator

---
+++ Frelocate/Controller/ForgottenPassword.swift
@@ -0,0 +1,9 @@
+//
+//  ForgottenPassword.swift
+//  Frelocate
+//
+//  Created by Rob Prior on 14/12/2017.
+//  Copyright © 2017 Rob Prior. All rights reserved.
+//
+
+import Foundation
---
+++ Frelocate/View/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Frelocate
+//
+//  Created by Rob Prior on 12/12/2017.
+//  Copyright © 2017 Rob Prior. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
